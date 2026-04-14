e5fe1b12fe65609edee970d9e540abb135aa9947
JoaoAndrade90 <joaomagalhaesandrade@gmail.com>
2026-04-14 12:47:20 +0100

    Finished Component Container

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,19 @@
 import "./styles/theme.css"
 import "./styles/global.css"
-import { Heading } from "./components/Heading.tsx"
-import { Timer as TimerIcon } from 'lucide-react'
+import { Container } from './components/Container';
+import { Heading } from "./components/Heading.tsx";
 
 export function App() {
     return (
     <>
-    <Heading>
-        Olá, mundo
-        <button>
-            <TimerIcon />
-        </button>
-    </Heading>
-    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Commodi delectus iure velit eveniet, quos nostrum maiores natus eaque optio nesciunt explicabo, nulla totam hic tempora accusantium voluptates saepe provident enim.</p>
+        
+        <Container>
+            <Heading>LOGO</Heading>
+        </Container>
+
+        <Container>
+           <Heading>MENU</Heading>
+        </Container>
     </>
     )
 }
--- src/components/Heading.tsx
+++ src/components/Heading.tsx
@@ -1,11 +1,8 @@
 import styles from "./Heading.module.css"
-import type { ReactNode } from 'react'
 
 type HeadingProps = {
     children: React.ReactNode;
 }
-
-// O componente Heading é um componente funcional que recebe props do tipo HeadingProps, que inclui uma propriedade children do tipo ReactNode. Ele retorna um elemento h1 com a classe CSS definida em styles.heading, e o conteúdo do children é renderizado dentro desse elemento h1.
 
 export function Heading(props: HeadingProps) {
     const { children } = props;
