c21574d07c5e2eea6fed56ed0f52871d52f96d6b
JoaoAndrade90 <joaomagalhaesandrade@gmail.com>
2026-04-23 16:37:55 +0100

    Fixed some bugs

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,15 @@
 import { Container } from "./components/Container/Container.tsx";
 import { Logo } from "./components/Logo/Logo.tsx";
-import { Menu } from "./components/Menu/Menu.tsx"
-import { CountDown } from "./components/CountDown/CountDown.tsx"
-import { DefaultInput } from './components/DefaultInput/DefaultInput';
+import { Menu } from "./components/Menu/Menu.tsx";
+import { CountDown } from "./components/CountDown/CountDown.tsx";
+import { DefaultInput } from "./components/DefaultInput/DefaultInput.tsx";
 import { Cycles } from "./components/Cycles/Cycles.tsx";
 import { DefaultButton } from "./components/DefaultButton/DefaultButton.tsx";
 import { Footer } from "./components/Footer/Footer.tsx";
-import { PlayCircleIcon, StopCircleIcon } from 'lucide-react';
+import { PlayCircleIcon } from 'lucide-react';
 
-import "./styles/theme.css"
-import "./styles/global.css"
+import "./styles/theme.css";
+import "./styles/global.css";
 
 export function App() {
     return (
@@ -30,7 +30,7 @@ export function App() {
             <form className="form">
                 <div className="formRow">
                     <DefaultInput 
-                    labelText="Qq coisa" 
+                    labelText= "task"
                     id="meuInput" 
                     type="text"
                     title="titulo"
--- src/components/Menu/Menu.tsx
+++ src/components/Menu/Menu.tsx
@@ -1,21 +1,81 @@
-import { HomeIcon, History, SettingsIcon, SunIcon } from "lucide-react";
-import styles from "./Menu.module.css"
+import {
+  HistoryIcon,
+  HouseIcon,
+  MoonIcon,
+  SettingsIcon,
+  SunIcon,
+} from 'lucide-react';
+import styles from './Menu.module.css';
+import { useState, useEffect } from 'react';
+
+type AvailableThemes = 'dark' | 'light';
 
 export function Menu() {
-    return (
-        <nav className={styles.menu}>
-            <a className={styles.menuLink} href="">
-                <HomeIcon></HomeIcon>
-            </a>
-            <a className={styles.menuLink} href="">
-                <History></History>
-            </a>
-            <a className={styles.menuLink} href="">
-                <SettingsIcon></SettingsIcon>
-            </a>
-            <a className={styles.menuLink} href="">
-                <SunIcon></SunIcon>
-            </a>
-        </nav>
-    )
+  const [theme, setTheme] = useState<AvailableThemes>(() => {
+    const storageTheme =
+      (localStorage.getItem('theme') as AvailableThemes) || 'dark';
+    return storageTheme;
+  });
+
+  const nextThemeIcon = {
+    dark: <SunIcon />,
+    light: <MoonIcon />,
+  };
+
+  function handleThemeChange(
+    event: React.MouseEvent<HTMLAnchorElement, MouseEvent>,
+  ) {
+    event.preventDefault();
+
+    setTheme(prevTheme => {
+      const nextTheme = prevTheme === 'dark' ? 'light' : 'dark';
+      return nextTheme;
+    });
+  }
+
+  useEffect(() => {
+    document.documentElement.setAttribute('data-theme', theme);
+    localStorage.setItem('theme', theme);
+  }, [theme]);
+
+  return (
+    <nav className={styles.menu}>
+      <a
+        className={styles.menuLink}
+        href='#'
+        aria-label='Ir para a Home'
+        title='Ir para a Home'
+      >
+        <HouseIcon />
+      </a>
+
+      <a
+        className={styles.menuLink}
+        href='#'
+        aria-label='Ver Histórico'
+        title='Ver Histórico'
+      >
+        <HistoryIcon />
+      </a>
+
+      <a
+        className={styles.menuLink}
+        href='#'
+        aria-label='Configurações'
+        title='Configurações'
+      >
+        <SettingsIcon />
+      </a>
+
+      <a
+        className={styles.menuLink}
+        href='#'
+        aria-label='Mudar Tema'
+        title='Mudar Tema'
+        onClick={handleThemeChange}
+      >
+        {nextThemeIcon[theme]}
+      </a>
+    </nav>
+  );
 }
