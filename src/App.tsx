ecfb5c64c028eeb7888b7ad04db7be015527eabf
JoaoAndrade90 <joaomagalhaesandrade@gmail.com>
2026-04-28 22:12:24 +0100

    To prevent prop drilling, some props and types were deleted.

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,23 @@
-import { AboutPomodoro } from './pages/AboutPomodoro/index';
-
 import "./styles/theme.css";
 import "./styles/global.css";
+import { Home } from "./pages/Home/index";
+import { useState } from "react";
+import type { TaskStateModel } from "./models/TaskStateModel.ts";
+
+const initialState: TaskStateModel = {
+  tasks: [],
+  secondsRemaining: 0,
+  formattedSecondsRemaining: "00:00",
+  activeTask: null,
+  currentCycle: 0,
+  config: {
+    workTime: 25,
+    shortBreakTime: 5,
+    longBreakTime: 15,
+  },
+};
 
 export function App() {
-    return (
-        <AboutPomodoro></AboutPomodoro>
-    )
+  const [state, setState] = useState(initialState);
+  return <Home></Home>
 }
--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,19 +1,18 @@
-import { MainTemplate } from '../../templates/MainTemplates/index';
-import { Container } from '../../components/Container/Container';
-import { CountDown } from '../../components/CountDown/CountDown.tsx';
-import { MainForm } from '../../components/MainForm/MainForm';
+import { MainTemplate } from "../../templates/MainTemplates/index";
+import { Container } from "../../components/Container/Container";
+import { CountDown } from "../../components/CountDown/CountDown.tsx";
+import { MainForm } from "../../components/MainForm/MainForm";
 
 export function Home() {
-    return (
-        <MainTemplate>
-            <Container>
-                <CountDown></CountDown>
-            </Container>
+  return ( 
+    <MainTemplate>
+      <Container>
+        <CountDown></CountDown>
+      </Container>
 
-            <Container>
-                <MainForm></MainForm>
-            </Container>
-        </MainTemplate>
-
-    )
-}
+      <Container>
+        <MainForm></MainForm>
+      </Container>
+    </MainTemplate> 
+  )
+};
